803f1c4ddca1f2a7850ea65986443610663db225
tao.wang <wangtaodd@gmail.com>
2018-04-11 12:30:18 +0200

    update some solutions

---
+++ 172. Factorial Trailing Zeroes.py
@@ -0,0 +1,1 @@
+"""Given an integer n, return the number of trailing zeroes in n!.Note: Your solution should be in logarithmic time complexity."""class Solution(object):    def trailingZeroes(self, n):        """        :type n: int        :rtype: int        """        res=0        while n>0:            res+=n//5            n=n//5        return res
--- 575_Distribute Candies.py
+++
@@ -1,6 +0,0 @@
-candies = [0, 1, 2, 3, 3, 4]
-candies_set = set(candies)
-print(len(candies_set))
-print(len(candies))
-count = min(len(candies_set) + 1, (len(candies) + 1) // 2)
-print(count)
